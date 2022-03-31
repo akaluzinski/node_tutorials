95c5874de23500839a08d0436bbfd56d1e0dcff3
Adrian Kaluzinski <kaluza@gmail.com>
2022-03-31 20:29:24 +0200

    Access temperature from API

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,20 @@ const request = require('request');
 
 const ApiKey = ''
 
-const query = 'New%20York';
+const query = 'Krakow,Poland';
 
 const url = `http://api.weatherstack.com/current?access_key=${ApiKey}&query=${query}`;
 
-request({url}, (error, {body}) => {
-    console.log(body);
+const options = {
+    url,
+    json: true
+}
+
+request(options, (error, { body }) => {
+    if (error != null){
+        throw Error('Sorry', error);
+    }
+    const { current: { feelslike, temperature }} = body;
+    const message = `Current temperature in ${query} is ${temperature} but feels like ${feelslike}`;
+    console.log(message);
 })
