0220f7d36da95516b100ca36e10ca33b99dc7196
Adrian Kaluzinski <kaluza@gmail.com>
2022-05-10 19:58:16 +0200

    Add user signing out #13

--- notez-app/src/routers/auth.js
+++ notez-app/src/routers/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require("../models/user");
+const {auth} = require("../middleware/auth");
 const authRouter = new express.Router();
 
 authRouter.post('/auth/login', async (req, res) => {
@@ -11,6 +12,21 @@ authRouter.post('/auth/login', async (req, res) => {
         res.send({ user, token });
     } catch (error) {
         res.status(403).send();
+    }
+});
+
+authRouter.post('/auth/logout', auth, async (req, res) => {
+    try {
+        console.log('Logout ', req.token);
+        req.user.tokens = req.user.tokens.filter((token) => {
+            if(token.token === req.token) {
+                console.log('Match, logout', token.token);
+            }
+           return token.token !== req.token;
+        });
+        res.send('OK');
+    } catch (error) {
+        res.status(500).send({ error: 'Unable to logout' });
     }
 });
 
