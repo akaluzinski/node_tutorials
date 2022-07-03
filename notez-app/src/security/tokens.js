2324d87a1e6d53526911d19dde71040c4bbe05e5
Adrian Kaluzinski <kaluza@gmail.com>
2022-07-03 11:17:23 +0200

    Move JWT dev key file to env variable

--- notez-app/src/security/tokens.js
+++ notez-app/src/security/tokens.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const process = require('process');
 
-const privateKey = 'secrettokentthatwillbeoverridensoon';
+const privateKey = process.env.JWT_KEY;
 const sendGridApiKey = process.env.SEND_GRID_KEY || '';
 
 async function generateToken(payload, expiresIn) {
